6a98e71e292d99c303a69df3e9725750df95a229
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2022-09-30 11:01:57 +0300

    Initial Commit

---
+++ NewsPapier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsPapier
+//
+//  Created by Abdurrahman Gazi Yavuz on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
