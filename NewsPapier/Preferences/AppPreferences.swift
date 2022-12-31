de9e30428e50ff0ad9c674cf163c6348215fe139
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2022-12-31 14:41:24 +0300

    More arch

---
+++ NewsPapier/Preferences/AppPreferences.swift
@@ -0,0 +1,11 @@
+//
+//  Preferences.swift
+//  NewsPapier
+//
+//  Created by Abdurrahman Gazi Yavuz on 12/31/22.
+//
+
+import Foundation
+
+#warning("App preferences for Light / Dark mode")
+#warning("App preferences for Language / Region etc.")
