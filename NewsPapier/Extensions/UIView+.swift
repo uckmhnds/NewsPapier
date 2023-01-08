f87e3bcf4903deebeb8e58dc772a6fd82585a0be
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2023-01-08 23:07:04 +0300

    More generic UIView anchor func is created

--- NewsPapier/Extensions/UIView+.swift
+++ NewsPapier/Extensions/UIView+.swift
@@ -8,24 +8,64 @@
 import UIKit
 
 extension UIView {
-    #warning("More generic anchor including height width anchors and constants")
-    public func anchor(top: NSLayoutYAxisAnchor,
-                       leading: NSLayoutXAxisAnchor,
-                       bottom: NSLayoutYAxisAnchor,
-                       trailing: NSLayoutXAxisAnchor,
-                       padding: UIEdgeInsets = .zero) {
+    
+    public func anchor(top: NSLayoutYAxisAnchor? = nil,
+                       leading: NSLayoutXAxisAnchor? = nil,
+                       bottom: NSLayoutYAxisAnchor? = nil,
+                       trailing: NSLayoutXAxisAnchor? = nil,
+                       padding: UIEdgeInsets = .zero,
+                       height: CGFloat? = nil,
+                       width: CGFloat? = nil,
+                       centerX: NSLayoutXAxisAnchor? = nil,
+                       centerY: NSLayoutYAxisAnchor? = nil) {
         
-        topAnchor.constraint(equalTo: top,
-                             constant: padding.top).isActive = true
+        if let top = top{
+            
+            topAnchor.constraint(equalTo: top,
+                                 constant: padding.top).isActive = true
+            
+        }
         
-        bottomAnchor.constraint(equalTo: bottom,
-                                constant: -padding.bottom).isActive = true
+        if let leading = leading{
+            
+            leadingAnchor.constraint(equalTo: leading,
+                                    constant: padding.left).isActive = true
+            
+        }
         
-        leadingAnchor.constraint(equalTo: leading,
-                                 constant: padding.left).isActive = true
+        if let bottom = bottom{
+            
+            bottomAnchor.constraint(equalTo: bottom,
+                                    constant: -padding.bottom).isActive = true
+            
+        }
         
-        trailingAnchor.constraint(equalTo: trailing,
-                            constant: -padding.right).isActive = true
+        if let trailing = trailing{
+            
+            trailingAnchor.constraint(equalTo: trailing,
+                                    constant: -padding.right).isActive = true
+            
+        }
+        
+        if let height = height{
+            
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+            
+        }
+        
+        if let width = width{
+            
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+            
+        }
+        
+        if let centerX = centerX{
+            centerXAnchor.constraint(equalTo: centerX).isActive = true
+        }
+        
+        if let centerY = centerY{
+            centerYAnchor.constraint(equalTo: centerY).isActive = true
+        }
         
     }
     
