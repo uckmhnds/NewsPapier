24a0c599dc0ef8d92bfdb0b33a2a095ce20bffca
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2023-01-10 23:40:41 +0300

    Swipre gesture extension for init() method is implemented

--- NewsPapier/Extensions/UIGesture+.swift
+++ NewsPapier/Extensions/UIGesture+.swift
@@ -9,11 +9,41 @@ import UIKit
 
 extension UITapGestureRecognizer{
     
-    public convenience init(target: Any?, action: Selector?, delegate: UIGestureRecognizerDelegate? = nil, name: String? = nil) {
+    public convenience init(target: Any?,
+                            action: Selector?,
+                            delegate: UIGestureRecognizerDelegate? = nil,
+                            name: String? = nil)
+    {
         self.init(target: target, action: action)
         
         if let delegate = delegate{
             self.delegate = delegate
+        }
+        
+        if let name = name{
+            self.name = name
+        }
+        
+    }
+    
+}
+
+extension UISwipeGestureRecognizer{
+    
+    public convenience init(target: Any?,
+                            action: Selector?,
+                            delegate: UIGestureRecognizerDelegate? = nil,
+                            direction: UISwipeGestureRecognizer.Direction? = nil,
+                            name: String? = nil)
+    {
+        self.init(target: target, action: action)
+        
+        if let delegate = delegate{
+            self.delegate = delegate
+        }
+        
+        if let direction = direction{
+            self.direction = direction
         }
         
         if let name = name{
