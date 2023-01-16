4debdc91e89c1a0cec735e1d8bb0d19b644df4c9
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2023-01-16 21:47:46 +0300

    Dummy onboarding swipeable view

--- NewsPapier/Controller/OnboardingViewController.swift
+++ NewsPapier/Controller/OnboardingViewController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 
-class OnboardingViewController: UIViewController {
+
+class OnboardingTestCell: UICollectionViewCell {
     
-    lazy var button: UIButton = {
+    static let identifier   = "OnboardingTestCell"
+    
+    private lazy var button: UIButton = {
         
         let action = UIAction { _ in
             
@@ -18,36 +21,228 @@ class OnboardingViewController: UIViewController {
         }
         
         let button = UIButton(primaryAction: action)
-//        let button = UIButton(primaryAction: action)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = Radius.r2
         button.backgroundColor = .red
         return button
     }()
+    
+    private func applyConstraints(){
+        
+        button.anchor(height: 80, width: 200, centerX: centerXAnchor, centerY: centerYAnchor)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.backgroundColor     = .systemGray3
+        
+//        contentView.addSubview(button)
+        
+//        applyConstraints()
+        
+    }
 
+}
+
+
+class OnboardingRegionCell: UICollectionViewCell {
+    
+    static let identifier   = "OnboardingRegionCell"
+    
+    private lazy var button: UIButton = {
+        
+        let action = UIAction { _ in
+            
+            UserPreferences.defaults.set(true, forKey: "Test")
+            
+        }
+        
+        let button = UIButton(primaryAction: action)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = Radius.r2
+        button.backgroundColor = .red
+        return button
+    }()
+    
+    private func applyConstraints(){
+        
+        button.anchor(height: 80, width: 200, centerX: centerXAnchor, centerY: centerYAnchor)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.backgroundColor     = .systemGray6
+        
+        contentView.addSubview(button)
+        
+        applyConstraints()
+        
+    }
+
+}
+
+
+class OnboardingViewController: UIViewController {
+    
+    private let numberOfPages : Int     = 2
+    
+    private let marginBetweenCell : Double = 0
+    
+    private let collectionView: UICollectionView = {
+        
+        let layout      = UICollectionViewFlowLayout()
+        
+        layout.scrollDirection = .horizontal
+        
+        let view        = UICollectionView(frame: .zero, collectionViewLayout:  layout)
+        
+        view.register(OnboardingRegionCell.self, forCellWithReuseIdentifier: OnboardingRegionCell.identifier)
+        view.register(OnboardingTestCell.self, forCellWithReuseIdentifier: OnboardingTestCell.identifier)
+        
+        return view
+    }()
+    
+    private let prevButton: UIButton = {
+        let button      = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints    = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        button.addTarget(nil, action: #selector(prevButtonEvent), for: .touchUpInside)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button      = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints    = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.systemPink, for: .normal)
+        button.addTarget(nil, action: #selector(nextButtonEvent), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var pageControl : UIPageControl = {
+        let pc                              = UIPageControl()
+        pc.numberOfPages                    = self.numberOfPages
+        pc.currentPage                      = 0
+        pc.currentPageIndicatorTintColor    = .systemPink
+        pc.pageIndicatorTintColor           = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+        return pc
+    }()
+    
+    fileprivate func setBottomControls() {
+        
+        let bottomControlStackView  = UIStackView(arrangedSubviews: [self.prevButton, self.pageControl, self.nextButton])
+        
+        bottomControlStackView.translatesAutoresizingMaskIntoConstraints    = false
+        bottomControlStackView.distribution = .fillEqually
+        
+        self.view.addSubview(bottomControlStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc private func nextButtonEvent() {
+        
+        let nextIndex = min(self.pageControl.currentPage + 1, self.numberOfPages - 1)
+        let indexPath = IndexPath(item: nextIndex, section: 0)
+        self.pageControl.currentPage = nextIndex
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    @objc private func prevButtonEvent() {
+        
+        let nextIndex = max(self.pageControl.currentPage - 1, 0)
+        let indexPath = IndexPath(item: nextIndex, section: 0)
+        self.pageControl.currentPage = nextIndex
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemGray
+        view.backgroundColor    = .systemRed
         
-        view.addSubview(button)
+        view.addSubview(collectionView)
+        
+        collectionView.isPagingEnabled  = true
+        
+        collectionView.delegate         = self
+        collectionView.dataSource       = self
+        
+        setBottomControls()
+        
+//        view.addSubview(button)
         
     }
     
     override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+        super.viewWillLayoutSubviews()
         
-        button.anchor(height: 80, width: 200, centerX: view.centerXAnchor, centerY: view.centerYAnchor)
+        collectionView.frame        = view.bounds
+    }
+
+
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfPages
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        print("here")
+        print(indexPath.item % 2 == 0)
+        
+        guard let cell = indexPath.item % 2 == 0 ? collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingRegionCell.identifier, for: indexPath) as? OnboardingRegionCell : collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingTestCell.identifier, for: indexPath) as? OnboardingTestCell else { return UICollectionViewCell() }
+        
+//        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        
+//        cell.backgroundColor    = indexPath.item % 2 == 0 ? .systemGray3 : .systemGray4
+        
+        return cell
     }
-    */
 
+}
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return marginBetweenCell
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let scrollOffsetX   = targetContentOffset.pointee.x
+        let frameWidth      = view.frame.width
+        
+        pageControl.currentPage = Int(scrollOffsetX / frameWidth)
+        
+    }
+    
 }
