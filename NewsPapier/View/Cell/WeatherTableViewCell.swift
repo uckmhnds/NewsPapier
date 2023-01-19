c41efb1a1f87715fe11000f7bcde85edbcef8f3c
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2023-01-19 11:53:54 +0300

    Weather VC, table view and tableview cell declarations

---
+++ NewsPapier/View/Cell/WeatherTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherTableViewCell.swift
+//  NewsPapier
+//
+//  Created by Abdurrahman Gazi Yavuz on 1/19/23.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell{
+    static let identifier = "WeatherTableViewCell"
+}
---
+++ NewsPapier/View/Table/WeatherTableView.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherTableView.swift
+//  NewsPapier
+//
+//  Created by Abdurrahman Gazi Yavuz on 1/19/23.
+//
+
+import Foundation
