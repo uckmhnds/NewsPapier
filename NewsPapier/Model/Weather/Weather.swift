4a78ac48f3b2c61b8fe2e142a81c97a1fe7698eb
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2023-01-19 11:25:15 +0300

    Weather data is implemented

---
+++ NewsPapier/Model/Weather/Weather.swift
@@ -0,0 +1,56 @@
+//
+//  Weather.swift
+//  NewsPapier
+//
+//  Created by Abdurrahman Gazi Yavuz on 1/18/23.
+//
+
+import Foundation
+
+public struct WeatherResponse: Codable, Hashable{
+    
+    let name: String
+    let weather: [Weather]
+    let main: Temp
+    
+    var minTemp: String {
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 0
+        
+        formatter.numberStyle = .decimal
+
+        let celcius = main.temp_min - 277.15
+        
+        return formatter.string(from: celcius as NSNumber) ?? "n/a"
+    }
+    
+    var maxTemp: String {
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 0
+        
+        formatter.numberStyle = .decimal
+
+        let celcius = main.temp_max - 277.15
+        
+        return formatter.string(from: celcius as NSNumber) ?? "n/a"
+    }
+}
+
+
+public struct Weather: Codable, Hashable{
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+
+public struct Temp: Codable, Hashable{
+    let temp: Double
+    let temp_min: Double
+    let temp_max: Double
+}
