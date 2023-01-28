3fc45aec7f5c667fde8e72b2481a635d7e53f13e
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2023-01-28 22:51:13 +0300

    Weather Table View implemented

--- NewsPapier/View/Table/WeatherTableView.swift
+++ NewsPapier/View/Table/WeatherTableView.swift
@@ -5,4 +5,23 @@
 //  Created by Abdurrahman Gazi Yavuz on 1/19/23.
 //
 
-import Foundation
+import UIKit
+
+final class WeatherTableView: UITableView{
+    
+    public init() {
+        
+        super.init(frame: .zero, style: .plain)
+        
+        self.register(WeatherTableViewCell.self, forCellReuseIdentifier: WeatherTableViewCell.identifier)
+        
+        backgroundColor = Theme.primaryBackground
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
