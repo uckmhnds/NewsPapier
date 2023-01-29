db8dcb38cbb2fa2883be32f44f8e848e91f6a960
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2023-01-29 22:01:05 +0300

    weather view controller methods, variables implementation

--- NewsPapier/Controller/WeatherViewController/WeatherViewController.swift
+++ NewsPapier/Controller/WeatherViewController/WeatherViewController.swift
@@ -8,12 +8,79 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    
+    private let tableView = WeatherTableView()
+    
+    // MARK: - Network
+    
+    var weatherResponseDict = [Dictionary<WeatherCase, WeatherResponse>.Element]()
+    
+    private var isLoading: Bool = true
+    private let dispatchGroup = DispatchGroup()
+    
+    private func addSubviews(){
+        view.addSubview(tableView)
+    }
+    
+    private func delegateMethods(){
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    private func loadLocalNews(){
+        
+        for weather in WeatherCase.allCases{
+            
+            self.dispatchGroup.enter()
+            
+            DispatchQueue.main.async {
+                
+                if let response = DecodeLocal.shared.fetch(fileName: weather.code) as WeatherResponse?
+                {
+                    let fetchedData: Dictionary<WeatherCase, WeatherResponse>.Element = (key: weather, value: response)
+                    self.weatherResponseDict.append(fetchedData)
+                }
+                
+                self.dispatchGroup.leave()
+            }
+            
+        }
+        
+        self.dispatchGroup.notify(queue: .main){
+            
+            self.isLoading = false
+            self.tableView.reloadData()
+            
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.loadLocalNews()
+        self.addSubviews()
+        self.delegateMethods()
+        
+        
+        title = Preferences.weatherViewControllerTitle
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+        
+//        tableView.anchor(top: view.bottomAnchor,
+//                         leading: view.leadingAnchor,
+//                         bottom: view.bottomAnchor,
+//                         trailing: view.trailingAnchor)
+        
+    }
+    
     
 
     /*
@@ -27,3 +94,4 @@ class WeatherViewController: UIViewController {
     */
 
 }
+
